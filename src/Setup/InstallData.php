1111b8226908ab42d4158c095a97af8669bc2975
Alex Gusev <alex@flancer64.com>
2016-01-14 11:28:25 +0200

    MOBI-179 - Create BonusHybrid module and move content from BonusPersonal

---
+++ src/Setup/InstallData.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Populate DB schema with module's initial data
+ * .
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+namespace Praxigento\Bonus\Hybrid\Setup;
+
+class InstallData extends \Praxigento\Core\Setup\Data\Base {
+
+    public function __construct() {
+        parent::__construct('Praxigento\Bonus\Hybrid\Lib\Setup\Data');
+    }
+
+}
