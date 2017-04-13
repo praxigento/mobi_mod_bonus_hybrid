44033e64e100c25f2d660ab93550571554b65611
Alex Gusev <alex@flancer64.com>
2017-04-13 17:51:09 +0300

    MOBI-685 - DCP API: grid entries for phase2

--- src/Repo/Query/Stats/Phase2/Builder.php
+++ src/Repo/Query/Stats/Phase2/Builder.php
@@ -2,12 +2,14 @@
 /**
  * User: Alex Gusev <alex@flancer64.com>
  */
+
 namespace Praxigento\BonusHybrid\Repo\Query\Stats\Phase2;
 
 use Praxigento\BonusBase\Data\Entity\Rank as Rank;
+use Praxigento\BonusHybrid\Config as Cfg;
 use Praxigento\BonusHybrid\Entity\Compression\Oi as Oi;
+use Praxigento\Core\Repo\Query\Expression as Expression;
 use Praxigento\Downline\Data\Entity\Customer as Cust;
-use Praxigento\Pv\Data\Entity\Sale as Pv;
 
 /**
  * Build query to get phase1 compressed PV/TV/OV statistics for the given calculation.
@@ -17,6 +19,7 @@ class Builder
 {
     /** Tables aliases */
     const AS_CUST = 'cst';
+    const AS_CUST_MAGE = 'mcst';
     const AS_PARENT = 'prn';
     const AS_RANK = 'rank';
     const AS_TREE = 'tree';
@@ -24,6 +27,7 @@ class Builder
     /** Columns aliases */
     const A_CUST_ID = Oi::ATTR_CUSTOMER_ID;
     const A_CUST_MLM_ID = 'customer_mlm_id';
+    const A_CUST_NAME = 'name';
     const A_DEPTH = Oi::ATTR_DEPTH;
     const A_OV_MAX = Oi::ATTR_OV_LEG_MAX;
     const A_OV_OTHER = Oi::ATTR_OV_LEG_OTHERS;
@@ -48,10 +52,12 @@ class Builder
         $asTree = self::AS_TREE;
         $asRank = self::AS_RANK;
         $asCust = self::AS_CUST;
+        $asCustM = self::AS_CUST_MAGE;
         $asPrnt = self::AS_PARENT;
 
         /* SELECT FROM prxgt_bon_hyb_cmprs_ptc */
         $tbl = $this->resource->getTableName(Oi::ENTITY_NAME);
+        $as = $asTree;
         $cols = [
             self::A_CUST_ID => Oi::ATTR_CUSTOMER_ID,
             self::A_PARENT_ID => Oi::ATTR_PARENT_ID,
@@ -63,31 +69,46 @@ class Builder
             self::A_OV_SECOND => Oi::ATTR_OV_LEG_SECOND,
             self::A_OV_OTHER => Oi::ATTR_OV_LEG_OTHERS
         ];
-        $result->from([$asTree => $tbl], $cols);
+        $result->from([$as => $tbl], $cols);
+
+        /* LEFT JOIN customer_entity */
+        $tbl = $this->resource->getTableName(Cfg::ENTITY_MAGE_CUSTOMER);
+        $as = $asCustM;
+        $fldFirst = "`$asCustM`.`" . Cfg::E_CUSTOMER_A_FIRSTNAME . "`";
+        $fldLast = "`$asCustM`.`" . Cfg::E_CUSTOMER_A_LASTNAME . "`";
+        $exp = new Expression("CONCAT($fldFirst, ' ', $fldLast)");
+        $cols = [
+            self::A_CUST_NAME => $exp
+        ];
+        $on = $asCustM . '.' . Cfg::E_CUSTOMER_A_ENTITY_ID . '=' . $asTree . '.' . Oi::ATTR_CUSTOMER_ID;
+        $result->joinLeft([$as => $tbl], $on, $cols);
 
         /* LEFT JOIN prxgt_bon_base_rank */
         $tbl = $this->resource->getTableName(Rank::ENTITY_NAME);
+        $as = $asRank;
         $cols = [
             self::A_RANK => Rank::ATTR_CODE
         ];
-        $on = $asRank. '.' . Rank::ATTR_ID. '=' . $asTree . '.' . Oi::ATTR_RANK_ID;
-        $result->joinLeft([$asRank => $tbl], $on, $cols);
+        $on = $asRank . '.' . Rank::ATTR_ID . '=' . $asTree . '.' . Oi::ATTR_RANK_ID;
+        $result->joinLeft([$as => $tbl], $on, $cols);
 
         /* LEFT JOIN prxgt_dwnl_customer (for customer MLM ID) */
         $tbl = $this->resource->getTableName(Cust::ENTITY_NAME);
+        $as = $asCust;
         $cols = [
             self::A_CUST_MLM_ID => Cust::ATTR_HUMAN_REF
         ];
         $on = $asCust . '.' . Cust::ATTR_CUSTOMER_ID . '=' . $asTree . '.' . Oi::ATTR_CUSTOMER_ID;
-        $result->joinLeft([$asCust => $tbl], $on, $cols);
+        $result->joinLeft([$as => $tbl], $on, $cols);
 
         /* LEFT JOIN prxgt_dwnl_customer (for parent MLM ID) */
         $tbl = $this->resource->getTableName(Cust::ENTITY_NAME);
+        $as = $asPrnt;
         $cols = [
             self::A_PARENT_MLM_ID => Cust::ATTR_HUMAN_REF
         ];
         $on = $asPrnt . '.' . Cust::ATTR_CUSTOMER_ID . '=' . $asTree . '.' . Oi::ATTR_PARENT_ID;
-        $result->joinLeft([$asPrnt => $tbl], $on, $cols);
+        $result->joinLeft([$as => $tbl], $on, $cols);
 
         return $result;
     }
