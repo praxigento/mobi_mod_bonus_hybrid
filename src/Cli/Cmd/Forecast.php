a950449368f293eb683210562aa9d39c9c8e6387
Alex Gusev <alex@flancer64.com>
2017-05-02 16:44:54 +0300

    MOBI-688 - Add Forecast calculation

--- src/Cli/Cmd/Forecast.php
+++ src/Cli/Cmd/Forecast.php
@@ -53,51 +53,6 @@ class Forecast
             $req = new \Praxigento\BonusHybrid\Service\Calc\Forecast\Request();
             $resp = $this->callCalcForecast->exec($req);
 
-            /* get balances on the end of the previous period */
-            $qCloseBegin = $this->qbldBalClose->getSelectQuery();
-            $bind = [
-                QBalanceClose::BIND_ASSET_TYPE_ID => 3,
-                QBalanceClose::BIND_MAX_DATE => '20170228'
-            ];
-            $rowsBegin = $this->conn->fetchAll($qCloseBegin, $bind);
-
-            /* get balances on the end of this period */
-            $qCloseBegin = $this->qbldBalClose->getSelectQuery();
-            $bind = [
-                QBalanceClose::BIND_ASSET_TYPE_ID => 3,
-                QBalanceClose::BIND_MAX_DATE => '20170310'
-            ];
-            $rowsEnd = $this->conn->fetchAll($qCloseBegin, $bind);
-
-
-            /* compose PV delta for period */
-            $bal = [];
-            $sum = 0;
-            foreach ($rowsEnd as $row) {
-                $customerId = $row[QBalanceClose::A_CUST_ID];
-                $accId = $row[QBalanceClose::A_ACC_ID];
-                $balanceClose = $row[QBalanceClose::A_BALANCE];
-                $data = new \Flancer32\Lib\Data();
-                $data->set('accountId', $accId);
-                $data->set('customerId', $customerId);
-                $data->set('balanceClose', $balanceClose);
-                $data->set('balanceOpen', 0);
-                $data->set('turnover', $balanceClose);
-                $bal[$customerId] = $data;
-            }
-            /* add opening balance and delta */
-            foreach ($rowsBegin as $row) {
-                $customerId = $row[QBalanceClose::A_CUST_ID];
-                $balanceOpen = $row[QBalanceClose::A_BALANCE];
-                $data = $bal[$customerId];
-                $balanceClose = $data->get('balanceClose');
-                $turnover = ($balanceClose - $balanceOpen);
-                $data->set('balanceOpen', $balanceOpen);
-                $data->set('turnover', $turnover);
-                $bal[$customerId] = $data;
-                $sum += $turnover;
-            }
-
 //            $this->conn->commit();
             $this->conn->rollBack();
         } catch (\Throwable $e) {
--- src/Service/Calc/Forecast.php
+++ src/Service/Calc/Forecast.php
@@ -5,21 +5,25 @@
 
 namespace Praxigento\BonusHybrid\Service\Calc;
 
+use Praxigento\BonusHybrid\Config as Cfg;
+
 class Forecast
     extends \Praxigento\Core\Service\Base\Call
     implements \Praxigento\BonusHybrid\Service\Calc\IForecast
 {
+    protected $callBalanceGetTurnover;
     /** @var  \Praxigento\Core\Tool\IPeriod */
     protected $toolPeriod;
 
     public function __construct(
         \Praxigento\Core\Fw\Logger\App $logger,
         \Magento\Framework\ObjectManagerInterface $manObj,
-        \Praxigento\Core\Tool\IPeriod $toolPeriod
+        \Praxigento\Core\Tool\IPeriod $toolPeriod,
+        \Praxigento\Accounting\Service\Balance\Get\ITurnover $callBalanceGetTurnover
     ) {
         parent::__construct($logger, $manObj);
         $this->toolPeriod = $toolPeriod;
-
+        $this->callBalanceGetTurnover = $callBalanceGetTurnover;
     }
 
     public function exec(\Praxigento\BonusHybrid\Service\Calc\Forecast\Request $req)
@@ -27,8 +31,54 @@ class Forecast
         $result = new \Praxigento\BonusHybrid\Service\Calc\Forecast\Response();
         $this->_logger->info("'Forecast' calculation is started.");
 
+        /* get calculation period (begin, end dates) */
+        list($dateFrom, $dateTo) = $this->getPeriod();
+
+        /* get PV turnover for period */
+        $reqTurnover = new \Praxigento\Accounting\Service\Balance\Get\Turnover\Request ();
+        $reqTurnover->assetTypeCode = Cfg::CODE_TYPE_ASSET_PV;
+        $reqTurnover->dateFrom = $dateFrom;
+        $reqTurnover->dateTo = $dateTo;
+        $respTurnover = $this->callBalanceGetTurnover->exec($reqTurnover);
+
+        /* extract only positive turnovers */
+        $entries = $respTurnover->entries;
+        $positiveTurnover = [];
+        /** @var \Praxigento\Accounting\Service\Balance\Get\Turnover\Data\Entry $entry */
+        foreach ($entries as $entry) {
+            $turnover = $entry->turnover;
+            $customerId = $entry->customerId;
+            if ($turnover > Cfg::DEF_ZERO) {
+                $positiveTurnover[$customerId] = $entry;
+            }
+        }
+
+        /* perform calculation */
+
+        /* replace actual data in repository */
+
         $this->_logMemoryUsage();
         $this->_logger->info("'Forecast' calculation is completed.");
+        return $result;
+    }
+
+    /**
+     * Return 2 dates (period being/end): first day of the month and yesterday.
+     * @return array
+     */
+    protected function getPeriod()
+    {
+        $today = $this->toolPeriod->getPeriodCurrent();
+        $yesterday = $this->toolPeriod->getPeriodPrev($today);
+        $month = $this->toolPeriod->getPeriodCurrent(null, 0, \Praxigento\Core\Tool\IPeriod::TYPE_MONTH);
+        $begin = $this->toolPeriod->getPeriodFirstDate($month);
+        $end = $yesterday;
+
+        /* TODO: remove it */
+        $begin = '20170301';
+        $end = '20170310';
+
+        $result = [$begin, $end];
         return $result;
     }
 }
