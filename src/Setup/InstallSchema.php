d5be5b98af63430a57d56d1541a9e36ee95fce52
Alex Gusev <alex@flancer64.com>
2017-05-02 17:31:14 +0300

    MOBI-708 - Create DB structure for un-compressed downline reports

--- src/Setup/InstallSchema.php
+++ src/Setup/InstallSchema.php
@@ -3,13 +3,14 @@
  * Create DB schema.
  * User: Alex Gusev <alex@flancer64.com>
  */
+
 namespace Praxigento\BonusHybrid\Setup;
 
+use Praxigento\BonusHybrid\Entity\Cache\Downline\Plain as CacheDwnlPlain;
 use Praxigento\BonusHybrid\Entity\Cfg\Override as CfgOverride;
 use Praxigento\BonusHybrid\Entity\Cfg\Param as CfgParam;
 use Praxigento\BonusHybrid\Entity\Compression\Oi as OiCompress;
 use Praxigento\BonusHybrid\Entity\Compression\Ptc as PtcCompress;
-use Praxigento\BonusHybrid\Entity\Config as HybridCfg;
 use Praxigento\BonusHybrid\Entity\Registry\Pto as RegPto;
 use Praxigento\BonusHybrid\Entity\Registry\SignupDebit as SignupDebit;
 
@@ -21,6 +22,11 @@ class InstallSchema extends \Praxigento\Core\Setup\Schema\Base
         $pathToFile = __DIR__ . '/../etc/dem.json';
         $pathToNode = '/dBEAR/package/Praxigento/package/Bonus/package/Hybrid';
         $demPackage = $this->_toolDem->readDemPackage($pathToFile, $pathToNode);
+
+        /* Cache Downline Plain */
+        $entityAlias = CacheDwnlPlain::ENTITY_NAME;
+        $demEntity = $demPackage->get('package/Cache/package/Downline/entity/Plain');
+        $this->_toolDem->createEntity($entityAlias, $demEntity);
 
         /* Config Override */
         $entityAlias = CfgOverride::ENTITY_NAME;
