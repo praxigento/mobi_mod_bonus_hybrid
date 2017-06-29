a0aa3d602337de804f2dc85fc9d86767fac7e294
Alex Gusev <alex@flancer64.com>
2017-06-29 12:52:01 +0300

    MOBI-777 - Repositories and queries

--- src/Repo/Data/Entity/Retro/Downline/Compressed/Phase1.php
+++ src/Repo/Data/Entity/Retro/Downline/Compressed/Phase1.php
@@ -9,6 +9,7 @@ namespace Praxigento\BonusHybrid\Repo\Data\Entity\Retro\Downline\Compressed;
  * Retrospective Downline Tree that is Compressed in Phase 1
  */
 class Phase1
+    extends \Praxigento\Core\Data\Entity\Base
 {
     const ATTR_CALC_ID = 'calc_id';
     const ATTR_CUSTOMER_ID = 'customer_ref';
@@ -19,4 +20,10 @@ class Phase1
     const ATTR_PV = 'pv';
     const ATTR_TV = 'tv';
     const ENTITY_NAME = 'prxgt_bon_hyb_retro_cmprs_phase1';
+
+    public function getPrimaryKeyAttrs()
+    {
+        $result = [self::ATTR_CALC_ID, self::ATTR_CUSTOMER_ID];
+        return $result;
+    }
 }
