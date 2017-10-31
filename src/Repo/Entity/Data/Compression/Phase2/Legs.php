8769a565c1c2c29b0301b7f82d882a528e5568c3
Alex Gusev <alex@flancer64.com>
2017-10-31 15:26:05 +0200

    MOBI-971 - Save links to customers & qual. data to 'prxgt_bon_hyb_cmprs_ph2_legs'

--- src/Repo/Entity/Data/Compression/Phase2/Legs.php
+++ src/Repo/Entity/Data/Compression/Phase2/Legs.php
@@ -14,10 +14,15 @@ class Legs
     extends \Praxigento\Core\Data\Entity\Base
 {
     const ATTR_CALC_REF = 'calc_ref';
+    const ATTR_CUST_MAX_REF = 'cust_max_ref';
     const ATTR_CUST_REF = 'cust_ref';
+    const ATTR_CUST_SECOND_REF = 'cust_second_ref';
     const ATTR_LEG_MAX = 'leg_max';
     const ATTR_LEG_OTHERS = 'leg_others';
     const ATTR_LEG_SECOND = 'leg_second';
+    const ATTR_PV_QUAL_MAX = 'pv_qual_max';
+    const ATTR_PV_QUAL_OTHER = 'pv_qual_other';
+    const ATTR_PV_QUAL_SECOND = 'pv_qual_second';
     const ENTITY_NAME = 'prxgt_bon_hyb_cmprs_ph2_legs';
 
     /**
@@ -32,9 +37,27 @@ class Legs
     /**
      * @return integer
      */
+    public function getCustMaxRef()
+    {
+        $result = parent::get(self::ATTR_CUST_MAX_REF);
+        return $result;
+    }
+
+    /**
+     * @return integer
+     */
     public function getCustRef()
     {
         $result = parent::get(self::ATTR_CUST_REF);
+        return $result;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getCustSecondRef()
+    {
+        $result = parent::get(self::ATTR_CUST_SECOND_REF);
         return $result;
     }
 
@@ -75,6 +98,33 @@ class Legs
     }
 
     /**
+     * @return float
+     */
+    public function getPvQualMax()
+    {
+        $result = parent::get(self::ATTR_PV_QUAL_MAX);
+        return $result;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPvQualOther()
+    {
+        $result = parent::get(self::ATTR_PV_QUAL_OTHER);
+        return $result;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPvQualSecond()
+    {
+        $result = parent::get(self::ATTR_PV_QUAL_SECOND);
+        return $result;
+    }
+
+    /**
      * @param integer $data
      */
     public function setCalcRef($data)
@@ -85,9 +135,25 @@ class Legs
     /**
      * @param integer $data
      */
+    public function setCustMaxRef($data)
+    {
+        parent::set(self::ATTR_CUST_MAX_REF, $data);
+    }
+
+    /**
+     * @param integer $data
+     */
     public function setCustRef($data)
     {
         parent::set(self::ATTR_CUST_REF, $data);
+    }
+
+    /**
+     * @param integer $data
+     */
+    public function setCustSecondRef($data)
+    {
+        parent::set(self::ATTR_CUST_SECOND_REF, $data);
     }
 
     /**
@@ -112,6 +178,31 @@ class Legs
     public function setLegSecond($data)
     {
         parent::set(self::ATTR_LEG_SECOND, $data);
+    }
+
+
+    /**
+     * @param float $data
+     */
+    public function setPvQualMax($data)
+    {
+        parent::set(self::ATTR_PV_QUAL_MAX, $data);
+    }
+
+    /**
+     * @param float $data
+     */
+    public function setPvQualOther($data)
+    {
+        parent::set(self::ATTR_PV_QUAL_OTHER, $data);
+    }
+
+    /**
+     * @param float $data
+     */
+    public function setPvQualSecond($data)
+    {
+        parent::set(self::ATTR_PV_QUAL_SECOND, $data);
     }
 
 }
