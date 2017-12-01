5d14f1b9883d689d7e7621e17c8fa985f5c4db02
Alex Gusev <alex@flancer64.com>
2017-12-01 20:43:50 +0200

    MOBI-689 Add "Unqualified" calculation

--- src/Service/Calc/Inactive/Collect.php
+++ src/Service/Calc/Inactive/Collect.php
@@ -81,11 +81,14 @@ class Collect
          */
         $ctx->set(self::CTX_OUT_SUCCESS, false);
         /* get dependent calculation data */
-        list($writeOffPeriod, $writeOffCalc, $collectCalc) = $this->getCalcData($periodEnd);
-        $calcId = $writeOffCalc->getId();
-        $tree = $this->repoBonDwnl->getByCalcId($calcId);
+        list($writeOffPeriod, $writeOffCalc, $writeOffCalcPrev, $collectCalc) = $this->getCalcData($periodEnd);
+        $writeOffCalcId = $writeOffCalc->getId();
+        $writeOffCalcIdPrev = $writeOffCalcPrev->getId();
+        $tree = $this->repoBonDwnl->getByCalcId($writeOffCalcId);
+        /** TODO: create query & get stats for the previous period */
         $prevStat = [];
-        $this->calc($tree, $prevStat);
+        $stats = $this->calc($tree, $prevStat);
+        $this->saveStats($stats);
         /* mark process as successful */
         $ctx->set(self::CTX_OUT_SUCCESS, true);
         $this->logger->info("Inactive Stats Collection calculation is completed.");
@@ -106,11 +109,37 @@ class Collect
         $this->procPeriodGet->exec($ctx);
         /** @var \Praxigento\BonusBase\Repo\Entity\Data\Period $writeOffPeriod */
         $writeOffPeriod = $ctx->get(SPeriodGetDep::CTX_OUT_BASE_PERIOD_DATA);
-        /** @var \Praxigento\BonusBase\Repo\Entity\Data\Calculation $collectCalc */
+        /** @var \Praxigento\BonusBase\Repo\Entity\Data\Calculation $writeOffCalc */
         $writeOffCalc = $ctx->get(SPeriodGetDep::CTX_OUT_BASE_CALC_DATA);
         /** @var \Praxigento\BonusBase\Repo\Entity\Data\Calculation $collectCalc */
         $collectCalc = $ctx->get(SPeriodGetDep::CTX_OUT_DEP_CALC_DATA);
-        $result = [$writeOffPeriod, $writeOffCalc, $collectCalc];
+        /**
+         * Get previous write off period to access inactive stats history.
+         */
+        $periodPrev = $writeOffPeriod->getDstampBegin();
+        $ctx = new \Praxigento\Core\Data();
+        $ctx->set(SPeriodGetDep::CTX_IN_BASE_TYPE_CODE, Cfg::CODE_TYPE_CALC_PV_WRITE_OFF);
+        $ctx->set(SPeriodGetDep::CTX_IN_DEP_TYPE_CODE, Cfg::CODE_TYPE_CALC_INACTIVE_COLLECT);
+        $ctx->set(SPeriodGetDep::CTX_IN_PERIOD_END, $periodPrev);
+        $ctx->set(SPeriodGetDep::CTX_IN_DEP_IGNORE_COMPLETE, true);
+        $this->procPeriodGet->exec($ctx);
+        /** @var \Praxigento\BonusBase\Repo\Entity\Data\Calculation $writeOffCalcPrev */
+        $writeOffCalcPrev = $ctx->get(SPeriodGetDep::CTX_OUT_BASE_CALC_DATA);
+        /**
+         * Compose result.
+         */
+        $result = [$writeOffPeriod, $writeOffCalc, $writeOffCalcPrev, $collectCalc];
         return $result;
+    }
+
+    /**
+     * @param \Praxigento\BonusHybrid\Repo\Entity\Data\Downline\Inactive[] $stats
+     */
+    private function saveStats($stats)
+    {
+        /** @var \Praxigento\BonusHybrid\Repo\Entity\Data\Downline\Inactive $stat */
+        foreach ($stats as $stat) {
+            $this->repoInact->create($stat);
+        }
     }
 }
