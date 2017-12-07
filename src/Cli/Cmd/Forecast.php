f4294752c3dfeaa566c571760dfe3d78bcb3af51
Alex Gusev <alex@flancer64.com>
2017-12-07 13:28:54 +0200

    MOBI-1026 DCP no rows to show

--- src/Cli/Cmd/Forecast.php
+++ src/Cli/Cmd/Forecast.php
@@ -19,12 +19,16 @@ class Forecast
     private $servCalcCompress;
     /** @var \Praxigento\BonusHybrid\Service\Calc\Forecast\IPlain */
     private $servCalcPlain;
+    /** @var \Praxigento\Downline\Api\Service\Snap\Clean */
+    private $servDwnlClean;
+    /** @var \Praxigento\Downline\Service\ISnap */
     private $servSnap;
 
     public function __construct(
         \Magento\Framework\ObjectManagerInterface $manObj,
         \Magento\Framework\App\ResourceConnection $resource,
         \Praxigento\Downline\Service\ISnap $servSnap,
+        \Praxigento\Downline\Api\Service\Snap\Clean $servDwnlClean,
         \Praxigento\BonusHybrid\Service\Calc\Forecast\IPlain $servCalcPlain,
         \Praxigento\BonusHybrid\Service\Calc\Forecast\ICompress $servCalcCompress
     )
@@ -37,8 +41,22 @@ class Forecast
         $this->resource = $resource;
         $this->conn = $this->resource->getConnection();
         $this->servSnap = $servSnap;
+        $this->servDwnlClean = $servDwnlClean;
         $this->servCalcPlain = $servCalcPlain;
         $this->servCalcCompress = $servCalcCompress;
+    }
+
+    private function buildSnaps()
+    {
+        $req = new \Praxigento\Downline\Service\Snap\Request\Calc();
+        $this->servSnap->calc($req);
+    }
+
+    private function cleanSnaps()
+    {
+        /* Clean up berfore rebuild. TODO: remove it after the last day of the snap will be processed correctly */
+        $req = new \Praxigento\Downline\Api\Service\Snap\Clean\Request();
+        $this->servDwnlClean->exec($req);
     }
 
     protected function execute(
@@ -47,11 +65,14 @@ class Forecast
     )
     {
         $output->writeln("<info>Start forecast calculations.<info>");
+        /* DDL statement TRUNCATE cannot be used inside transaction. */
+        $this->cleanSnaps();
+        /* perform the main processing */
         $this->conn->beginTransaction();
         try {
             $ctx = new \Praxigento\Core\Data();
             /* MOBI-1026: re-build downline snaps before calculations */
-            $this->rebuildSnaps();
+            $this->buildSnaps();
             /* ... then perform forecast calculations */
             $this->servCalcPlain->exec($ctx);
             $this->servCalcCompress->exec($ctx);
@@ -63,11 +84,5 @@ class Forecast
             $this->conn->rollBack();
         }
         $output->writeln('<info>Command is completed.<info>');
-    }
-
-    private function rebuildSnaps()
-    {
-        $req = new \Praxigento\Downline\Service\Snap\Request\Calc();
-        $this->servSnap->calc($req);
     }
 }
