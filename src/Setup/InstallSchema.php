1cc797eda7f8a7f2d7d7592e12bda02b305d41dd
Alex Gusev <alex@flancer64.com>
2017-12-31 21:59:19 +0200

    MOBI-1073 Clean up InstallScheme classes in modules

--- src/Setup/InstallSchema.php
+++ src/Setup/InstallSchema.php
@@ -11,59 +11,51 @@ use Praxigento\BonusHybrid\Repo\Entity\Data\Cfg\Param as CfgParam;
 use Praxigento\BonusHybrid\Repo\Entity\Data\Compression\Phase1\Transfer\Pv as Phase1TransPv;
 use Praxigento\BonusHybrid\Repo\Entity\Data\Compression\Phase2\Legs as Phase2Legs;
 use Praxigento\BonusHybrid\Repo\Entity\Data\Downline as Dwnl;
-use Praxigento\BonusHybrid\Repo\Entity\Data\Downline\Qualification as DwnlQual;
 use Praxigento\BonusHybrid\Repo\Entity\Data\Downline\Inactive as DwnlInact;
+use Praxigento\BonusHybrid\Repo\Entity\Data\Downline\Qualification as DwnlQual;
 use Praxigento\BonusHybrid\Repo\Entity\Data\Registry\SignupDebit as SignupDebit;
 
 class InstallSchema
     extends \Praxigento\Core\App\Setup\Schema\Base
 {
-    protected function _setup()
+    protected function setup()
     {
         /** Read and parse JSON schema. */
         $pathToFile = __DIR__ . '/../etc/dem.json';
         $pathToNode = '/dBEAR/package/Praxigento/package/Bonus/package/Hybrid';
-        $demPackage = $this->_toolDem->readDemPackage($pathToFile, $pathToNode);
+        $demPackage = $this->toolDem->readDemPackage($pathToFile, $pathToNode);
 
         /* Downline Tree (common for actual/retro & plain/compressed cases)*/
-        $entityAlias = Dwnl::ENTITY_NAME;
         $demEntity = $demPackage->get('entity/Downline');
-        $this->_toolDem->createEntity($entityAlias, $demEntity);
+        $this->toolDem->createEntity(Dwnl::ENTITY_NAME, $demEntity);
 
         /* Config Override */
-        $entityAlias = CfgOverride::ENTITY_NAME;
         $demEntity = $demPackage->get('package/Config/entity/Override');
-        $this->_toolDem->createEntity($entityAlias, $demEntity);
+        $this->toolDem->createEntity(CfgOverride::ENTITY_NAME, $demEntity);
 
         /* Config Param */
-        $entityAlias = CfgParam::ENTITY_NAME;
         $demEntity = $demPackage->get('package/Config/entity/Parameter');
-        $this->_toolDem->createEntity($entityAlias, $demEntity);
+        $this->toolDem->createEntity(CfgParam::ENTITY_NAME, $demEntity);
 
         /* Registry Sign Up Volume Debit */
-        $entityAlias = SignupDebit::ENTITY_NAME;
         $demEntity = $demPackage->get('package/Registry/entity/SignUpVolumeDebit');
-        $this->_toolDem->createEntity($entityAlias, $demEntity);
+        $this->toolDem->createEntity(SignupDebit::ENTITY_NAME, $demEntity);
 
         /* Compression / Phase I / Transfer / PV */
-        $entityAlias = Phase1TransPv::ENTITY_NAME;
         $demEntity = $demPackage->get('package/Compression/package/Phase1/package/Transfer/entity/Pv');
-        $this->_toolDem->createEntity($entityAlias, $demEntity);
+        $this->toolDem->createEntity(Phase1TransPv::ENTITY_NAME, $demEntity);
 
         /* Compression / Phase II / Legs */
-        $entityAlias = Phase2Legs::ENTITY_NAME;
         $demEntity = $demPackage->get('package/Compression/package/Phase2/entity/Legs');
-        $this->_toolDem->createEntity($entityAlias, $demEntity);
+        $this->toolDem->createEntity(Phase2Legs::ENTITY_NAME, $demEntity);
 
         /* Downline / Inactive */
-        $entityAlias = DwnlInact::ENTITY_NAME;
         $demEntity = $demPackage->get('package/Downline/entity/Inactive');
-        $this->_toolDem->createEntity($entityAlias, $demEntity);
+        $this->toolDem->createEntity(DwnlInact::ENTITY_NAME, $demEntity);
 
         /* Downline / Qualification */
-        $entityAlias = DwnlQual::ENTITY_NAME;
         $demEntity = $demPackage->get('package/Downline/entity/Qualification');
-        $this->_toolDem->createEntity($entityAlias, $demEntity);
+        $this->toolDem->createEntity(DwnlQual::ENTITY_NAME, $demEntity);
 
     }
 
