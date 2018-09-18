e2fe045237753f03fce3e5d00b9116cb3633486d
Alex Gusev <alex@flancer64.com>
2018-09-18 20:22:36 +0300

    SAN-335 Last updated time in distributors dashboard September 7

--- Cli/Forecast.php
+++ Cli/Forecast.php
@@ -5,8 +5,12 @@
 
 namespace Praxigento\BonusHybrid\Cli;
 
-use Praxigento\BonusBase\Repo\Query\Period\Calcs\GetLast\ByCalcTypeCode\Builder as QGetLastCalc;
+use Praxigento\BonusBase\Api\Service\Period\Calc\Get\Last\Request as ACalcLastRequest;
+use Praxigento\BonusBase\Api\Service\Period\Calc\Get\Last\Response as ACalcLastResponse;
+use Praxigento\BonusBase\Repo\Data\Period as EPeriod;
+use Praxigento\BonusHybrid\Config as Cfg;
 use Praxigento\BonusHybrid\Service\Calc\Forecast\Plain as APlain;
+use Praxigento\Core\Api\Helper\Period as HPeriod;
 
 /**
  * Daily calculation to forecast results on final bonus calc.
@@ -24,15 +28,20 @@ class Forecast
     private $servCalcPlain;
     /** @var \Praxigento\Downline\Api\Service\Snap\Calc */
     private $servSnapCalc;
-    /** @var \Praxigento\BonusBase\Repo\Query\Period\Calcs\GetLast\ByCalcTypeCode\Builder */
-    private $qGetLastCalc;
-
+    /** @var \Praxigento\BonusBase\Api\Service\Period\Calc\Get\Last */
+    private $servCalcGetLast;
+    /** @var \Praxigento\BonusBase\Repo\Dao\Period */
+    private $daoPeriod;
+    /** @var \Praxigento\BonusBase\Repo\Dao\Type\Calc */
+    private $daoTypeCalc;
     public function __construct(
         \Magento\Framework\ObjectManagerInterface $manObj,
         \Praxigento\Core\Api\App\Repo\Transaction\Manager $manTrans,
-        \Praxigento\BonusBase\Repo\Query\Period\Calcs\GetLast\ByCalcTypeCode\Builder $qGetLastCalc,
+        \Praxigento\BonusBase\Repo\Dao\Period $daoPeriod,
+        \Praxigento\BonusBase\Repo\Dao\Type\Calc $daoTypeCalc,
         \Praxigento\Core\Api\Helper\Period $hlpPeriod,
         \Praxigento\Downline\Api\Service\Snap\Calc $servSnapCalc,
+        \Praxigento\BonusBase\Api\Service\Period\Calc\Get\Last $servCalcGetLast,
         \Praxigento\BonusHybrid\Service\Calc\Forecast\Plain $servCalcPlain,
         \Praxigento\BonusHybrid\Service\Calc\Forecast\Compress $servCalcCompress
     ) {
@@ -42,17 +51,33 @@ class Forecast
             'Daily calculations to forecast results on final bonus calc.'
         );
         $this->manTrans = $manTrans;
-        $this->qGetLastCalc = $qGetLastCalc;
+        $this->daoPeriod = $daoPeriod;
+        $this->daoTypeCalc = $daoTypeCalc;
         $this->hlpPeriod = $hlpPeriod;
         $this->servSnapCalc = $servSnapCalc;
+        $this->servCalcGetLast = $servCalcGetLast;
         $this->servCalcPlain = $servCalcPlain;
         $this->servCalcCompress = $servCalcCompress;
     }
 
     private function calcPeriods()
     {
-        $query = $this->qGetLastCalc->build();
-        QGetLastCalc::A_CALC_ID;
+        $req = new ACalcLastRequest();
+        $req->setCalcTypeCode(Cfg::CODE_TYPE_CALC_PV_WRITE_OFF);
+        /** @var ACalcLastResponse $resp */
+        $resp = $this->servCalcGetLast->exec($req);
+        $period = $resp->getPeriod();
+        $dsEnd = $period->getDstampEnd();
+        $periodNext = $this->hlpPeriod->getPeriodNext($dsEnd, HPeriod::TYPE_MONTH);
+        /* compare last calculated period with current period */
+        $periodPrev = null;
+        $periodCurrent = $this->hlpPeriod->getPeriodCurrent(null, 0, HPeriod::TYPE_MONTH);
+        /* period after the last calculated is not equal to current period - we need to calc both */
+        if ($periodNext != $periodCurrent) {
+            $periodPrev = $periodNext;
+        }
+
+        return [$periodPrev, $periodCurrent];
     }
 
     protected function execute(
@@ -67,6 +92,8 @@ class Forecast
             $this->rebuildDwnlSnaps();
             /* should we calculate one or two periods? */
             list($periodPrev, $periodCurr) = $this->calcPeriods();
+            /* clean up forecast calculations data */
+            $this->cleanForecastCalcs();
             /* if previous period is not closed yet */
             if ($periodPrev) {
                 $ctx = new \Praxigento\Core\Data();
@@ -92,6 +119,15 @@ class Forecast
         $output->writeln('<info>Command is completed.<info>');
     }
 
+    private function cleanForecastCalcs()
+    {
+        $typeIdPlain = $this->daoTypeCalc->getIdByCode(Cfg::CODE_TYPE_CALC_FORECAST_PLAIN);
+        $typeIdCompress = $this->daoTypeCalc->getIdByCode(Cfg::CODE_TYPE_CALC_FORECAST_PHASE1);
+        $byPlain = EPeriod::A_CALC_TYPE_ID . '=' . (int)$typeIdPlain;
+        $byCompress = EPeriod::A_CALC_TYPE_ID . '=' . (int)$typeIdCompress;
+        $where = "($byPlain) OR ($byCompress)";
+        $this->daoPeriod->delete($where);
+    }
     /**
      * MOBI-1026: re-build downline snaps before calculations.
      *
